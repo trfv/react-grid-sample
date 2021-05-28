5651d6d01af5e30562801f8d1b30c5be7f158bfa
senda_y <35182743+trfv@users.noreply.github.com>
2021-05-28 10:35:04 +0900

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom";
+import { App } from "./App.jsx";
+import "./index.css";
+
+ReactDOM.render(<StrictMode><App /></StrictMode>, document.getElementById("root"));
